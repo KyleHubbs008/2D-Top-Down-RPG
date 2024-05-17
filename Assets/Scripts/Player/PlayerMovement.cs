8d2d8b9092cf04d08838898a71bc24fb22e7605b
KyleHubbs008 <khubb008@odu.edu>
2024-05-17 16:07:32 -0400

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 movement;
+    
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+
+    private void Awake() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update() 
+    {
+        movement.Set(InputManager.Movement.x, InputManager.Movement.y);
+
+        /**
+         * Since we are setting the velocity directly, we don't need to multiply 
+         * the movement vector by Time.deltaTime.
+         */
+        rb.velocity = movement * moveSpeed;
+
+        animator.SetFloat(horizontal, movement.x);
+        animator.SetFloat(vertical, movement.y);
+    }
+}
